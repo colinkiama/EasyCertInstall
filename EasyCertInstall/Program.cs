2d3303b2ed51eee0d2cb6169a58e1c3354fe3318
Colin Kiama <colinkiama@hotmail.co.uk>
2018-07-15 01:59:07 +0100

    Program now tries to install certificate from signed package when there is now certificate file available

--- EasyCertInstall/Program.cs
+++ EasyCertInstall/Program.cs
@@ -5,6 +5,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Diagnostics;
 
 namespace EasyCertInstall
 {
@@ -12,16 +13,16 @@ namespace EasyCertInstall
     {
         static void Main(string[] args)
         {
+            
             string currentDirectory = Directory.GetCurrentDirectory();
 
             Console.WriteLine($"Current directory is: {currentDirectory}");
             var files = Directory.GetFiles(currentDirectory,"*.cer");
 
-            string certFileName = "";
 
-            if (files.Count() > 0)
+            if (files.Length > 0)
             {
-                certFileName = files.First();
+                string certFileName = files.First();
                 X509Certificate2 cert = new X509Certificate2(certFileName);
                 using (X509Store store = new X509Store(StoreName.TrustedPeople, StoreLocation.LocalMachine))
                 {
@@ -34,11 +35,34 @@ namespace EasyCertInstall
 
             else
             {
+                InstallCertFromSignedPackage();
                 Console.WriteLine("Certificate file is not available in the current directory.");
                 Console.WriteLine("Please launch this program in the directory where the certifcate file you want to install is located");
             }
 
             Console.ReadLine();
+        }
+
+        private static void InstallCertFromSignedPackage()
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            var files = Directory.GetFiles(currentDirectory, "*.appxbundle*");
+
+
+            if (files.Length > 0)
+            {
+                string signedPackagePath = files.First();
+                X509Certificate certFromSignedPkg = X509Certificate.CreateFromSignedFile(signedPackagePath);
+                X509Certificate2 cert = new X509Certificate2(certFromSignedPkg);
+                using (X509Store store = new X509Store(StoreName.TrustedPeople, StoreLocation.LocalMachine))
+                {
+                    store.Open(OpenFlags.ReadWrite);
+                    store.Add(cert);
+                }
+                Console.WriteLine($"Certificate for signed package: {signedPackagePath} has successfully been added to the certifcate store.");
+                Console.WriteLine("You can now easily install the app by double clicking the .appx/.appxbundle file");
+            }
+
         }
     }
 }
